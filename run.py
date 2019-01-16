8313bbd6c9b88952e3980c9d01e6f93f064d9fe6
Carlo Feliciano Aureus <cnaureus.dev@gmail.com>
2019-01-15 22:51:44 -0800

    clean

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ This module starts the application represented by app object.
 
 import sys
 from app import app
-import atexit
 
 if __name__ == '__main__':
     sys.exit(app.main(len(sys.argv), sys.argv))
